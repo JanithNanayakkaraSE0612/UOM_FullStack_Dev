7f3a3a26cbbadb826ca5ea198972173ea115dab3
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-04 22:51:40 +0530

    Added

---
+++ products-api/server.js
@@ -0,0 +1,74 @@
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+var cron = require('node-cron');
+var bodyParser = require("body-parser");
+const { request, response } = require("express");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let HTTP_PORT = 8000
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
+
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+});
+
+app.post("/api/customer/register/", (req, res, next) => {
+
+    try {
+        var errors = []
+
+        if (!req.body) {
+            errors.push("An invalid input");
+        }
+
+        const {
+            name,
+            address,
+            email,
+            dateOfBirth,
+            gender,
+            age,
+            cardHolderName,
+            cardNumber,
+            expiryDate,
+            cvv,
+            timeStamp
+        } = req.body;
+
+        var sql = 'INSERT INTO products (productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate) VALUES (?,?,?,?,?,?,?,?,?,?)'
+        var params = [productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate]
+        db.run(sql, params, function (err, result) {
+
+            if (err) {
+                res.status(400).json({ "error": err.message })
+                return;
+            } else {
+                res.json({
+                    "message": "success",
+                    "data": req.body,
+                    "id": this.lastID
+                })
+            }
+
+        });
+    } catch (E) {
+        res.status(400).send(E);
+    }
+});
+
+
+
+
+
+// Root path
+app.get("/", (req, res, next) => {
+    res.json({ "message": "University of Moratuwa" })
+});
